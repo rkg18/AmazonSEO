5821263b88644e6b3be3b0ad85e149a1b62c5ec4
rkg18 <rkg18@zips.uakron.edu>
2019-03-04 19:57:43 -0500

    Grab top 10 search results

--- script.py
+++ script.py
@@ -2,14 +2,15 @@
 # Input: Product Keyword
 # Output: PDF file with first page product info and what could be improved to rank
 
-from functions import *
 from amazon.api import AmazonAPI
+from key import *
 
 keywords='earphones'
 
-amazon = AmazonAPI('AKIAIDMLCQFWH64BE5KA','vNpc7x047m9ez18Zfw/90/s6jVj6okixTAr/3QNt','beyourshelves-20')
+amazon = AmazonAPI(newKey1,newKey2,'beyourshelves-20')
 
 # Gets first page results for top amazon products
 products = amazon.search_n(10,Keywords=keywords, SearchIndex='All')
 
-commonStrings(products)
+for i in products:
+    print(i)
