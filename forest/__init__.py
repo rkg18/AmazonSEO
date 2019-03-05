ba82c7e73876fbe1c95e157ba726b1bf0d75c0fe
rkg18 <rkg18@zips.uakron.edu>
2019-03-04 20:06:56 -0500

    Add Flask init files

---
+++ forest/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+from flask import render_template
+
+# Create Application
+
+def create_app(test_config=None):
+    # Creates Instance & Tells App where files are relative
+    app = Flask(__name__, instance_relative_config=True)
+
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return render_template('base.html')
+
+    return app
--- script.py
+++ script.py
@@ -5,9 +5,9 @@
 from amazon.api import AmazonAPI
 from key import *
 
-keywords='earphones'
+keywords='cell phone charger'
 
-amazon = AmazonAPI(newKey1,newKey2,'beyourshelves-20')
+amazon = AmazonAPI(newKey1,newKey2,'beyourshelves-20') # Inits API
 
 # Gets first page results for top amazon products
 products = amazon.search_n(10,Keywords=keywords, SearchIndex='All')
