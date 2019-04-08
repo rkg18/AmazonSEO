826ba67e9fe604d5ce7fcd45216f69f6749e58ec
rkg18 <rkg18@zips.uakron.edu>
2019-04-08 12:21:14 -0400

    Update about and add deployment files for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn "imprint:create_app()"
--- forest/__init__.py
+++ forest/__init__.py
@@ -9,6 +9,10 @@ def create_app(test_config=None):
     # Creates Instance & Tells App where files are relative
     app = Flask(__name__, instance_relative_config=True)
 
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
+
     from . import keyword
     app.register_blueprint(keyword.bp)
 
